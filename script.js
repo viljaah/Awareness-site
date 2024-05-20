2026204323a27128dc95eb02aeee821d1b509e3b
viljaah <you@example.com>
2024-05-20 10:34:23 +0200

    reduced motion and other fixes

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-gsap.to(".rain", {
-  y: "150vh",
-  duration: 8,
-});
+// https://dev.to/natclark/checking-for-reduced-motion-preference-in-javascript-4lp9
+const reducedMotion = window.matchMedia(
+  "(prefers-reduced-motion: reduce)"
+).matches;
+if (!reducedMotion) {
+  gsap.to(".rain", {
+    y: "150vh",
+    duration: 8,
+  });
+} else {
+  gsap.set(".rain", {
+    y: "150vh",
+  });
+}
 
 gsap.to(".sweater", {
   x: 120,
   duration: 7,
   repeat: -1,
 });
-
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -25,16 +34,16 @@ const target = document.querySelector(".animated-image");
 observer.observe(target);
 
 
-
+// here I got inspiration from another group when they presented in class
 function checkTurn() {
   if (window.innerWidth > window.innerHeight) {
-      document.querySelector('.turn').style.display = 'none';
+    document.querySelector(".turn").style.display = "none";
   } else {
-      document.querySelector('.turn').style.display = 'flex';
+    document.querySelector(".turn").style.display = "flex";
   }
 }
 
-window.addEventListener('resize', checkTurn);
-window.addEventListener('turnchange', checkTurn);
+window.addEventListener("resize", checkTurn);
+window.addEventListener("turnchange", checkTurn);
 
 checkTurn();
